c904f639c4de7fbf367cb0482b4ae45a75181a8f
Ryan Bueter <magnus010@bueters.com>
2017-08-05 00:17:08 -0600

    Initial commit

---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * Created by magnus010 on 8/5/2017.
+ */
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
